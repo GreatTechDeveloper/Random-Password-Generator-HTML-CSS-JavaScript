329e24803f7e02bc2698d1c4a086634d747f4628
mohammad shakiba <mohammmad1069@gmail.com>
2024-08-11 18:23:51 +0330

    simplified

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 const passwordBox = document.getElementById("password");
-const length = 12;
+const createPassword = document.getElementById("createPassword");
+const passwordLength = 12;
+const allChar =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_+=-/*.|{}';:,";
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "~`!@#$%^&*()_-=+/.,?';:[]{}";
+const copyPassword = document.getElementById("copyPassword");
 
-const allCharacters = upperCase + lowerCase + numbers + symbols;
-
-function createPassword() {
+createPassword.addEventListener("click", () => {
   let password = "";
-  password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += numbers[Math.floor(Math.random() * numbers.length)];
-  password += symbols[Math.floor(Math.random() * symbols.length)];
 
-  while (length > password.length) {
-    password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  while (password.length < passwordLength) {
+    password += allChar[Math.floor(Math.random() * allChar.length)];
   }
   passwordBox.value = password;
-}
+});
 
-// function copyPassword() {
-//   passwordBox.select();
-//   document.execCommand("copy");
-// }
-
-function copyPassword() {
-    const password = passwordBox.value;
-    navigator.clipboard.writeText(password).then(() => {
-      alert("Password copied to clipboard!");
-    }).catch(err => {
-      console.error("Could not copy text: ", err);
-    });
-  }
-
-  
-//dataset
+copyPassword.addEventListener("click", () => {
+  const password = passwordBox.value;
+  navigator.clipboard.writeText(password).then(()=>{
+    alert("Password copied to clipboard!")
+  }).catch(err=> {
+    console.error("Could not copy text:", err)
+  })
+});
